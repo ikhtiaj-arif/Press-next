45b41e7fb02633673a116c9f403c46ed2f8fe95b
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2026-07-21 15:26:36 +0600

    shadcn component library added and .env setup

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div  >
       <h3>Hello world Home page</h3>
 
-    
+      <Button className="bg-red">New Button</Button>
+
     </div>
   );
 }
